65c7082202ba2bb625dfba54e80d3014bcf84a73
FabjanElezi <fabioelezi285@gmail.com>
2026-05-03 03:11:02 +0200

    Revert scroll indicator to original bottom-8 position

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -155,7 +155,7 @@ export default function Hero({ portfolio }: Props) {
 
         {/* Scroll indicator */}
         <motion.div
-          className="absolute -bottom-4 left-1/2 -translate-x-1/2"
+          className="absolute bottom-8 left-1/2 -translate-x-1/2"
           animate={{ y: [0, 8, 0] }}
           transition={{ repeat: Infinity, duration: 2, ease: 'easeInOut' }}
         >
